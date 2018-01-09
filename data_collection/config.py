979678f20203af577ee05162f57e724ad8488fc6
Chloe <challierchloe@gmail.com>
2018-01-09 12:17:05 +0000

    projectfinal

---
+++ data_collection/config.py
@@ -0,0 +1,4 @@
+ckey = 'kCL37OFq4Kj7Wk2svmxdL7Wk6'
+csecret = '17kV3PiOFMuYrZSw2sD8bNctDzI32GoNAOOcvxJjlmyygPEdp9'
+atoken = '823596473371729920-BWnAjKYn2eWmxiVdDZ10DG7ZFyCVlP7'
+asecret = 'hC75WwYZfQtp58KioB46odzw670sfe45iQfBcSUgDDZYA'
